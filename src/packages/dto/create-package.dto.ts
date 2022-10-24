99dc5e2ddbad2e05e4f221d27d36ab35da596ae4
earlofurl <jackthename@gmail.com>
2022-10-24 10:24:06 -0700

    package create bug fix. add class-transformer

--- src/packages/dto/create-package.dto.ts
+++ src/packages/dto/create-package.dto.ts
@@ -1,10 +1,21 @@
+import { Type } from 'class-transformer';
+
 export class CreatePackageDto {
   tagId: string;
+
   sourcePackageId: string;
+
   inheritedLabTestIds: string;
+
   itemId: string;
+
+  @Type(() => Number)
   quantity: number;
+
   uomId: string;
+
+  @Type(() => Number)
   newParentQuantity: number;
+
   notes: string;
 }
